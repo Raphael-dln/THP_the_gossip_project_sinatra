69385da351112d42bea9ada25df772c54819a808
Raphael-dln <delannay.raphael@gmail.com>
2020-04-27 12:57:51 +0200

    1st part ex

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,7 +1,16 @@
+require_relative 'gossip'
+
 class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
 
-  
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+
+  post '/gossips/new/' do
+    Gossip.new(@author, @content).save
+  end
+
 end
