3f4cf62acad175990e767a4d868ddba42d8056d8
Raphael-dln <delannay.raphael@gmail.com>
2020-04-27 14:39:35 +0200

    End basics

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -2,28 +2,31 @@ require 'pry'
 require 'csv'
 
 class Gossip
-  attr_reader :author, :content
+  attr_accessor :author, :content
 
   def initialize(author, content)
     @author = author
     @content = content
   end
-  
+
+  # Sauvegarde chaque gossip dans un fichier csv, n'écrase pas les lignes ayant déjà du contenu
   def save
     CSV.open("./db/gossip.csv", "ab") do |csv|
       csv << [@author, @content]
     end
   end
 
+  # Enregistre tous les gossips dans un array
   def self.all
     all_gossips = []
-    CSV.read("./db/gossip.csv").each do |csv|
-      gossip = 
-
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      gossip = Gossip.new(csv_line[0], csv_line[1])
+      all_gossips << gossip
+    end
     return all_gossips
   end
 
-
+ 
 
 end
 
